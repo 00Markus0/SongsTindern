07384d71116ffb8c133fe541ed46bd255e5feb7e
ragor114 <69634980+ragor114@users.noreply.github.com>
2022-01-03 17:59:16 +0100

    tried to fix heroku crash again again

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function init() {
     // Access command line parameters from start command (see package.json)
     let appDirectory = process.argv[2], // folder with client files
         // appPort = process.argv[3], // port to use for serving static files
-        port = process.env.port || 80;
+        port = process.env.PORT || 3000;
     server = new AppServer(appDirectory);
     server.start(port);
 }
